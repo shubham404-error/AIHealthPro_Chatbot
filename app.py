3f88d549cf3b1b36a3e74c6c57f313f2330415c9
Shubham Tejani <64226852+shubham404-error@users.noreply.github.com>
2024-03-06 21:36:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from pathlib import Path
 import google.generativeai as genai
 st.set_page_config(page_title="AIHealthPro Chatbot", page_icon=":robot:")
+st.image("logo.png",width=100)
 st.title("AIHealthPro Chatbot")
-st.image("logo.png",width=200)
-st.write("AIHealthPro is a chatbot that can help you with your health questions.")
+#st.write("AIHealthPro is a chatbot that can help you with your health questions.")
+
+st.write('ChatBot is under development')
