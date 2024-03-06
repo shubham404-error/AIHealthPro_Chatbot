9516f829968a0075ac833df3d6d14f7a052008ee
Shubham Tejani <64226852+shubham404-error@users.noreply.github.com>
2024-03-06 21:34:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from pathlib import Path
 import google.generativeai as genai
 st.set_page_config(page_title="AIHealthPro Chatbot", page_icon=":robot:")
 st.title("AIHealthPro Chatbot")
-st.image("https://www.cdc.gov/healthcommunication/ToolsTemplates/Photos/HealthPro_508.gif",width=200)
+st.image("logo.png",width=200)
 st.write("AIHealthPro is a chatbot that can help you with your health questions.")
