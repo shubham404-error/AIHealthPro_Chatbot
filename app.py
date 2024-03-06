aad01785585e22b6b885420c4ea85ad2c2de9c99
Shubham Tejani <64226852+shubham404-error@users.noreply.github.com>
2024-03-06 21:36:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ st.image("logo.png",width=100)
 st.title("AIHealthPro Chatbot")
 #st.write("AIHealthPro is a chatbot that can help you with your health questions.")
 
-st.write('ChatBot is under development')
+st.write('⚠️ChatBot is under development🧑‍💻')
